9716ee53a77ad8b1f84419a26bb75fb6e960f09b
Beshr Alghalil <beshrghalil@protonmail.com>
2025-05-25 18:31:25 +0300

    feat(logging): add console handler

--- autolog/logging_config.py
+++ autolog/logging_config.py
@@ -21,25 +21,26 @@ def setup_logging():
     if CONSOLE_LOGGING environment variable is 'True' (default).
     """
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)  # Allow all levels to be processed
 
     formatter = logging.Formatter(
         "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
 
+    debug_mode = os.environ.get("AUTOLOG_DEBUG", "False") == "True"
+    log_level = logging.DEBUG if debug_mode else logging.INFO
+    logger.setLevel(log_level)
+
     # Console handler
-    if os.environ.get("CONSOLE_LOGGING", "True") == "True":
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-        debug_mode = os.environ.get("AUTOLOG_DEBUG", "False") == "True"
-        console_handler.setLevel(logging.DEBUG if debug_mode else logging.INFO)
-        logger.addHandler(console_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(log_level)
+    logger.addHandler(console_handler)
 
     # File handler
     log_file = LOGGING_FILE
     file_handler = RotatingFileHandler(
         log_file, maxBytes=10485760, backupCount=1
-    )  # 10MB per file, keep 5 backups
+    )  # 10MB per file, keep 1 backup
     file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(log_level)
     logger.addHandler(file_handler)
